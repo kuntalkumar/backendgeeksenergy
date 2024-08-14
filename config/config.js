0e7760a8c2743b2bdc554636ed824a6d6687e006
kuntalkumar <kuntalkumar789@gmail.com>
2024-08-14 14:43:38 +0530

    aaded

---
+++ config/config.js
@@ -0,0 +1,6 @@
+// config.js
+module.exports = {
+    mongoURI: "mongodb+srv://kuntalkumar789:kuntal98@cluster0.vigwezr.mongodb.net/practice",
+    jwtSecret: '12456', // Optional if using JWT
+  };
+  
